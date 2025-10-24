7c797497390a98012f0b9f3891c69e73a5bd9473
samwatson7 <30259128@blackpool.ac.uk>
2025-10-24 10:47:21 +0100

    Added function to read CSV and return dictionaries

--- app.py
+++ app.py
@@ -7,20 +7,29 @@ import csv, os
 from datetime import datetime 
 
 # Defining CSV  file path and column headers:
-    # CSV_PATH => path to the inventory CSV file 
-    # COLUMNS_HEADERS  => headers for the inventory CSV file (columns of inventory table)
+    # csv_path      => path to the inventory CSV file 
+    # column_header => headers for the inventory CSV file (columns of inventory table)
 
-CSV_PATH = "inventory.csv"
-COLUMNS_HEADERS  = ["item_identification", "item_name", "quantity", "unit", "name_of_inputter", "date"]
+csv_path      = "inventory.csv"
+column_header = ["item_identification", "item_name", "item_quantity", "item_unit", "name_of_inputter", "date_inputted"]
 
 # Function, that when called, checks the CSV file exists and has headers 
 # If the file is missing or empty, create it and write column headers
 
 def is_csv_ready(): 
-    """Create the CSV with a header if it doesn't exist or is empty."""  # Docstring      - Explians what function does 
-    if not os.path.exists(CSV_PATH) or os.path.getsize(CSV_PATH) == 0:   # if Statement   - Checks if CSV file doesn't exist or is empty
-        with open(CSV_PATH, "w", newline="", encoding="utf-8") as f:     # with statement - opens CSV file in write mode (creating CSV if it doesn't exist) 
-            csv.writer(f).writerow(COLUMNS_HEADERS)                      # Writes header row (Column names) to CSV
+    """Create the CSV with a header if CSV doesn't already exist / CSV is empty"""  # Docstring      => Explians what function does 
+    if not os.path.exists(csv_path) or os.path.getsize(csv_path) == 0:              # if Statement   => Checks if CSV file doesn't exist or is empty
+        with open(csv_path, mode="w", encoding="utf-8", newline="") as file:        # with statement => opens CSV file in write mode (creating CSV if it doesn't exist) 
+            csv.writer(file).writerow(column_header)                                # Writes header row (Column names) to CSV
 
-# Calling function to ensure CSV file exists when program starts 
-is_csv_ready()
+# Function, that when called, reads all inventory items in CSV file 
+# Function then returns a list of dictionaries => each row in CSV becomes a dictionary
+
+def get_inventory_items():
+    """Return all inventory items as a list of dictionaries"""            # Docstring        => Explains what the function does 
+    is_csv_ready()                                                        # Function Calling => calls is_csv_ready() fuction, ensuring CSV exists before inventory_read_all() function tries to read 
+    with open(csv_path, mode="r", encoding="utf-8", newline="") as file:  # with statement   => opens CSV file in read mode, preparing to read each row
+        reader = csv.DictReader(file)                                     # Tells function to read each row of CSV as a dictionary (name:value)
+        item_dictionary_list  = list(reader)                              # Collects all the item dictionaries, orders them in a list 
+        
+    return item_dictionary_list                                           # Function Result Return => Function reads all rows, returning them as list of dictionaries 
