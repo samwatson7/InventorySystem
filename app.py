ffd64f08cc2159241a8078e9e3a4e10980cbd749
samwatson7 <30259128@blackpool.ac.uk>
2025-10-24 12:26:30 +0100

    - Added make_next_item_id function

--- app.py
+++ app.py
@@ -6,12 +6,16 @@
 import csv, os 
 from datetime import datetime 
 
+
+
 # Defining CSV  file path and column headers:
     # csv_path      => path to the inventory CSV file 
     # column_header => headers for the inventory CSV file (columns of inventory table)
 
 csv_path      = "inventory.csv"
 column_header = ["item_identification", "item_name", "item_quantity", "item_unit", "name_of_inputter", "date_inputted"]
+
+
 
 # Function, that when called, checks the CSV file exists and has headers 
 # If the file is missing or empty, create it and write column headers
@@ -26,14 +30,39 @@ def is_csv_ready():
             writer = csv.writer(csv_file)                                            # creates a CSV writer => writes into CSV file 
             writer.writerow(column_header)                                           # Tells the writer to write the header row (column names) on CSV file 
 
+
+
 # Function, that when called, reads all inventory items in CSV file 
 # Function then returns a list of dictionaries => each row in CSV becomes a dictionary
 
 def item_dictionary_list():
     """Return all inventory items as a list of dictionaries"""            # Docstring        => Explains what the function does 
+
     is_csv_ready()                                                        # Function Calling => calls is_csv_ready() fuction, ensuring CSV exists before inventory_read_all() function tries to read 
+
     with open(csv_path, mode="r", encoding="utf-8", newline="") as file:  # with statement   => opens CSV file in read mode, preparing to read each row
         reader = csv.DictReader(file)                                     # Tells function to read each row of CSV as a dictionary (name:value)
         item_dictionary_list  = list(reader)                              # Collects all the item dictionaries, orders them in a list 
+
     return item_dictionary_list ()                                        # Function Result Return => Function reads all rows, returning them as list of dictionaries 
+
+
+# Function, that when called, checks all existing items in CSV, finding the highest ID
+#Function then assigns the next number
+
+def make_next_item_id():
+    """Return next available numeric ID for Inventory item awaiting ID"""  # Docstring        => Explains what the function does 
+
+    items = item_dictionary_list()                                         # Function calling => calls item_dictionary_list() which reads whole Inventory CSV file, returning list of dictionaries => saves function list to variable; items
+    highest_id = 0                                                         # Begins tracking highest ID found in items, initialised to zero 
+
+    for item in items:                                                     # Loop => this loop goes through (loops over) each item in the list (item in items)
+        try:                                                               # Try => Begins appempts to convert items ID to an integer, it won't crash if it cant
+            current_id = int(item.get("item_id", "0") or "0")              # Tries to get item_id from dictionary , if empty it uses zero => converting id to integer 
+            if current_id > highest_id:                                    # Checks if the current ID is the biggest its seen so far 
+                highest_id = current_id                                    # Updates highest_id if current_id is biggest so far
+        except ValueError:                                                 
+            continue                                                       # Where item_id isn't a number, the function skips the row
+    
+    return str(highest_id + 1)                                             # Function returns the next available ID as a string 
 
