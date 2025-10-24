445535a2a7f14641740ed376c5395b4d09047283
samwatson7 <30259128@blackpool.ac.uk>
2025-10-24 09:27:52 +0100

    Add CSV path and column headers constants

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-# importing necessary modules:
-# csv      => to read/write to CSV files
-# os       => enables python to interact with the operating system (e.g. checking files, creating folders, ect.)
-# datetime => enables me to work with dates and times in my code 
+# Importing necessary modules:
+    # csv      => to read/write to CSV files
+    # os       => enables python to interact with the operating system (e.g. checking files, creating folders, ect.)
+    # datetime => enables me to work with dates and times in my code 
 
 import csv, os 
 from datetime import datetime 
+
+ # Defining CSV  file path and column headers:
+    # CSV_PATH => path to the inventory CSV file 
+    # COLUMNS  => headers for the inventory CSV file (columns of inventory table)
+
+CSV_PATH = "inventory.csv"
+COLUMNS = ["item_id", "item_name", "quantity", "unit", "added_by", "date_added"]
