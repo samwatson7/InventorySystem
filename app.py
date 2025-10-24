eee8602edc3fc13dcc491225017fe6b1214b653c
samwatson7 <30259128@blackpool.ac.uk>
2025-10-24 09:17:48 +0100

    Set up imports for CSV, OS, and datetime

---
+++ app.py
@@ -0,0 +1,7 @@
+# importing necessary modules:
+# csv      => to read/write to CSV files
+# os       => enables python to interact with the operating system (e.g. checking files, creating folders, ect.)
+# datetime => enables me to work with dates and times in my code 
+
+import csv, os 
+from datetime import datetime 
